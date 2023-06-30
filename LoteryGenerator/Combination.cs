b5e348ad23754ae4e3c1348d601ba7d5dcc7b82e
jrochamartins <jrocha.gm@gmail.com>
2023-06-30 15:01:19 -0300

    Modificação de geração da key.

--- LoteryGenerator/Combination.cs
+++ LoteryGenerator/Combination.cs
@@ -1,7 +1,23 @@
-﻿namespace LoteryGenerator
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace LoteryGenerator
 {
     public class Combination : SortedSet<int>
     {
-        internal string Key => string.Join('.', this);
+        internal string Key
+        {
+            get
+            {
+                var source = Encoding.ASCII.GetBytes(string.Concat(this));
+                var sourceArray = MD5.HashData(source);
+
+                int i;
+                var sOutput = new StringBuilder(sourceArray.Length);
+                for (i = 0; i < sourceArray.Length - 1; i++)
+                    sOutput.Append(sourceArray[i].ToString("X2"));
+                return sOutput.ToString();
+            }
+        }
     }
 }
