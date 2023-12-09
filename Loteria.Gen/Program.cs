9367d680909591df0554e81370d37f5640bf20ec
José Rocha <jrocha.gm@gmail.com>
2023-12-09 15:35:21 -0300

    Verificador de resultados

--- Loteria.Gen/Program.cs
+++ Loteria.Gen/Program.cs
@@ -1,59 +1,49 @@
 ﻿using LoteryGenerator;
+using static System.Net.WebRequestMethods;
 
 Console.WriteLine("### GERADOR DE COMBINAÇÕES DE LOTERIA ###");
+
+// Parameters
 Console.Write("Quantas combinações (padão: 10)?  ");
 _ = int.TryParse(Console.ReadLine(), out var amount);
+if (amount <= 0) amount = 10;
+
 Console.Write("De quantos números, cada combinação (padão: 15)? ");
 _ = int.TryParse(Console.ReadLine(), out var of);
-Console.Write("De um total de quantos números (padão: 25)? ");
-_ = int.TryParse(Console.ReadLine(), out var from);
-
-if (amount <= 0) amount = 10;
-if (from <= 0) from = 25;
 if (of <= 0) of = 15;
 
-var concursoReferencia = new Result(new Combination { 01, 02, 03, 04, 07, 08, 09, 10, 16, 17, 19, 21, 22, 23, 24 });
-var combinations = new RandomCombinationSetFactory(of, from).Generate(amount).ToArray();
+Console.Write("De um total de quantos números (padão: 25)? ");
+_ = int.TryParse(Console.ReadLine(), out var from);
+if (from <= 0) from = 25;
 
-// Resultados
+// Generate
+var combinations = new RandomCombinationSetFactory(of, from)
+    .Generate(amount)
+    .ToArray();
+
+// Results
 Console.ForegroundColor = ConsoleColor.White;
 Console.WriteLine($"{Environment.NewLine}Resultados");
 Console.WriteLine($"{combinations.Length} combinações geradas, contendo {of} número{(of > 1 ? "s" : "")} em cada:");
+
+var reference = new Combination { 04, 06, 08, 09, 10, 12, 13, 14, 15, 16, 18, 19, 20, 21, 24 };
 
 var count = 0;
 foreach (var combination in combinations)
 {
     Console.ForegroundColor = count % 2 == 0 ? ConsoleColor.Green : ConsoleColor.Yellow;
     Console.Write(combination);
-    var hits = concursoReferencia.Hits(combination);
-    if (hits > 10)
+
+    var result = new ResultChecker(combination, reference);
+    if (result.PercentageOfSucess <= 70)
+        Console.WriteLine();
+    else
     {
         var oldCollor = Console.ForegroundColor;
         Console.ForegroundColor = ConsoleColor.Red;
-        Console.WriteLine($" -> {hits} acertos");
+        Console.WriteLine($" -> {result.Hits} acertos");
         Console.ForegroundColor = oldCollor;
     }
-    else
-        Console.WriteLine();
     count++;
 }
 Console.ForegroundColor = ConsoleColor.Gray;
-
-//// ## Estatísticas
-//Console.ForegroundColor = ConsoleColor.White;
-//Console.WriteLine("\nEstatísticas");
-//Console.Write("Quantidade de números utilizada:");
-//int count = 0;
-//foreach (var counts in sta.Counts)
-//{
-//    if (count % 10 == 0)
-//    {
-//        Console.WriteLine();
-//        Console.ForegroundColor = Console.ForegroundColor != ConsoleColor.DarkGreen ? ConsoleColor.DarkGreen : ConsoleColor.DarkYellow;
-//    }
-//    Console.Write($"[{counts.Key.ToString().PadLeft(2, '0')}: {counts.Value}] ");    
-//    count++;
-//}
-//Console.ForegroundColor = ConsoleColor.Gray;
-//Console.WriteLine();
-
---
+++ LoteryGenerator/ResultChecker.cs
@@ -0,0 +1,27 @@
+﻿namespace LoteryGenerator
+{
+    public class ResultChecker
+    {
+        public int Hits => _hits;
+        private readonly int _hits;
+
+        public double PercentageOfSucess => _percentageOfSucess;
+        private readonly double _percentageOfSucess;
+
+        public ResultChecker(Combination attempt, Combination result)
+        {
+            _hits = CalculateHits(attempt, result);
+            _percentageOfSucess = CalculatePercentageOfSucess(attempt);
+        }
+
+        private static int CalculateHits(Combination attempt, Combination result)
+        {
+            var attemptSet = new SortedSet<int>(attempt);
+            attemptSet.IntersectWith(result);
+            return attemptSet.Count;
+        }
+
+        public double CalculatePercentageOfSucess(Combination attempt) =>
+            _hits * 100 / attempt.Count;
+    }
+}
