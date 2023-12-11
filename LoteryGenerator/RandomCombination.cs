35c0f0379945f7279e46079be5c9fd546e3b8701
José Rocha <jrocha.gm@gmail.com>
2023-12-11 11:32:40 -0300

    Ajustes de performance e uso do framework

--- LoteryGenerator/RandomCombination.cs
+++ LoteryGenerator/RandomCombination.cs
@@ -2,11 +2,9 @@
 {
     public class RandomCombination : Combination
     {
-        private static readonly Random Rand = new();
-
         private readonly int _of;
         private readonly int _from;
-        private readonly ISet<int> _priority;
+        private readonly HashSet<int> _priority;
 
         public RandomCombination(int of, int from)
         {
@@ -15,13 +13,12 @@
 
             _of = of;
             _from = from;
-            _priority = new HashSet<int>();
+            _priority = [];
         }
 
         public RandomCombination With(ISet<int> priority)
         {
-            if (priority == null) 
-                throw new ArgumentNullException(nameof(priority));
+            ArgumentNullException.ThrowIfNull(priority);
 
             if (priority.Any())
                 _priority.UnionWith(priority);
@@ -37,10 +34,10 @@
                 if (fromSet.Count == 0)
                     fromSet.UnionWith(Enumerable.Range(1, _from));
 
-                if (!_priority.Any())
+                if (_priority.Count == 0)
                     _priority.UnionWith(fromSet);
 
-                var index = Rand.Next(_priority.Count);
+                var index = Random.Shared.Next(_priority.Count);
                 var value = _priority.ElementAt(index);
                 Add(value);
 
